08291b42aac8fa6d57ad6d54f0032f1ccd30b4a5
MiguelBorges <1231548@isep.ipp.pt>
2025-11-07 10:37:46 +0000

    CursoEmVideo - part3

--- Atividade1/CursoEmVideo/part3/script.js
+++ Atividade1/CursoEmVideo/part3/script.js
@@ -7,15 +7,34 @@ function verificar(){
     if (fano.value.length == 0 || (fano.value) > ano){
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
-        var fsex = document.getElementById('radsex')
+        var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
         if (fsex[0].checked){
             genero = 'Homem'
+            if (idade >= 0 && idade < 10){
+                res.innerHTML = `É uma criança com ${idade} anos.`
+            } else if (idade < 21){
+                res.innerHTML = `É um jovem com ${idade} anos.`
+            } else if (idade < 50) {
+                res.innerHTML = `É um adulto com ${idade} anos.`
+            } else {
+                res.innerHTML = `É um sénior com ${idade} anos.`
+            }
         }else if (fsex[1].checked){
             genero = 'Mulher'
+            if (idade >= 0 && idade < 10){
+                res.innerHTML = `É uma criança com ${idade} anos.`
+            } else if (idade < 21){
+                res.innerHTML = `É uma jovem com ${idade} anos.`
+            } else if (idade < 50) {
+                res.innerHTML = `É uma adulta com ${idade} anos.`
+            } else {
+                res.innerHTML = `É uma sénior com ${idade} anos.`
+            }
         }
-        res.innerHTML = `Detetamos ${genero} com ${idade} anos.`
     }
+   // res.innerHTML = `Detetamos ${genero} com ${idade}.`
+
  }
     
